b6e8a7efbeabd329fa30389ef8d2dbea0e21eca9
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-05-14 20:00:04 +0200

    changed padding and margin styling in the styled component of StyledParagraph

--- src/LoadingPage/styled.js
+++ src/LoadingPage/styled.js
@@ -9,14 +9,13 @@ export const StyledParagraph = styled.p`
   font-weight: 600;
   font-size: 36px;
   line-height: 43.2px;
-  padding: 0 0 120px;
-  margin: 0;
+  margin: 0 0 120px;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     font-weight: 500;
     font-size: 14px;
     line-height: 18.2px;
-    padding: 0 0 24px;
+    margin: 0 0 24px;
   }
 `;
 
