6d7d0bb6e1e696bfe6af2d26c070e672da4f3794
Sofya <sofigrekal@gmail.com>
2024-05-14 20:19:22 +0200

    feat: add ImagePlaceholder to ActorTile component

--- src/ActorTile/index.js
+++ src/ActorTile/index.js
@@ -1,9 +1,18 @@
+import { ReactComponent as PlaceholderIcon } from "../assets/placeholderActorIcon.svg";
+
+import { ImagePlaceholder } from "../ImagePlaceholder/styled";
 import { StyledActorTile, Image, ActorName } from "./styled";
 
 export const ActorTile = ({ poster, actorName }) => {
   return (
     <StyledActorTile>
-      <Image src={poster} alt="This actor photo" />{" "}
+      {poster ? (
+        <Image src={poster} alt="This actor photo" />
+      ) : (
+        <ImagePlaceholder>
+          <PlaceholderIcon />
+        </ImagePlaceholder>
+      )}
       <ActorName>{actorName}</ActorName>
     </StyledActorTile>
   );
