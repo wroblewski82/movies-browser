999d2cd6e3f4fe0ef14f0544ec1535f39db14eb2
Sofya <124407697+sofyahreksoftware@users.noreply.github.com>
2024-05-18 14:08:13 +0200

    feat: implement ErrorPage component
    * feat: implement ErrorPage component
    
    * refactor: simplify styles in Container, StyledHeader, StyledParagraph, and StyledLink, add Open Sans font to StyledLink

---
+++ src/ErrorPage/styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 180px 16px 32px;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    margin: auto;
+  }
+`;
+
+export const StyledHeader = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 43.2px;
+`;
+
+export const StyledParagraph = styled.p`
+  max-width: 426.53px;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 28.6px;
+  margin: 0 auto 40px;
+`;
+
+export const StyledLink = styled.a`
+  background-color: ${({ theme }) => theme.colors.cornflowerBlue};
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 5px;
+  padding: 16px 24px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 700;
+  font-size: 14px;
+  text-align: center;
+  line-height: 19px;
+  cursor: pointer;
+`;
