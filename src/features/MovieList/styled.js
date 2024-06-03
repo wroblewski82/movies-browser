e3180101e5948f00366186008f5cdcd01e67a097
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-06-03 20:46:36 +0200

    refactor: update styling in StyledMain and StyledHeader components

--- src/features/MovieList/styled.js
+++ src/features/MovieList/styled.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 export const StyledMain = styled.main`
   max-width: 1400px;
   margin: 0 auto;
-  padding: 16px;
+  padding: 0 16px;
 `;
 
 export const StyledHeader = styled.h1`
   font-size: 36px;
   line-height: 1.2;
+  margin-top: 56px;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     font-size: 18px;
