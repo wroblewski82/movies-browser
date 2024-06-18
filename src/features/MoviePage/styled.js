d2502023cca524b2e170969cc8784b2f089876a9
Sofya <sofigrekal@gmail.com>
2024-06-18 09:35:50 +0200

    fix: adjust gradient styling on poster

--- src/features/MoviePage/styled.js
+++ src/features/MoviePage/styled.js
@@ -7,55 +7,65 @@ export const PosterContainer = styled.div`
   display: inline-block;
   height: 770px;
   position: relative;
-
-  background: linear-gradient(
+  background-image: linear-gradient(
       270deg,
-      #000000 14.11%,
-      rgba(0, 0, 0, 0.873268) 15.08%,
-      rgba(0, 0, 0, 0.720529) 16.51%,
-      rgba(0, 0, 0, 0.294577) 19.99%,
-      rgba(0, 0, 0, 0.159921) 21.88%,
+      rgb(0, 0, 0) 14.11%,
+      rgba(0, 0, 0, 0.875) 15.08%,
+      rgba(0, 0, 0, 0.72) 16.51%,
+      rgba(0, 0, 0, 0.294) 19.99%,
+      rgba(0, 0, 0, 0.16) 21.88%,
       rgba(0, 0, 0, 0) 25.68%
     ),
     linear-gradient(
       90deg,
-      #000000 13.6%,
-      rgba(0, 0, 0, 0.984059) 14.58%,
-      rgba(0, 0, 0, 0.967732) 15.44%,
-      rgba(0, 0, 0, 0.865569) 16.3%,
-      rgba(0, 0, 0, 0.230315) 22.87%,
+      rgb(0, 0, 0) 13.6%,
+      rgba(0, 0, 0, 0.984) 14.58%,
+      rgba(0, 0, 0, 0.97) 15.44%,
+      rgba(0, 0, 0, 0.867) 16.3%,
+      rgba(0, 0, 0, 0.23) 22.87%,
       rgba(0, 0, 0, 0) 26.64%
     ),
     linear-gradient(
-      180deg,
-      #000000 0%,
-      rgba(0, 0, 0, 0.689555) 4.66%,
-      rgba(0, 0, 0, 0.439033) 9.34%,
-      rgba(0, 0, 0, 0.20628) 15.16%,
+      rgb(0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.69) 4.66%,
+      rgba(0, 0, 0, 0.44) 9.34%,
+      rgba(0, 0, 0, 0.208) 15.16%,
       rgba(0, 0, 0, 0) 24.22%
     ),
     linear-gradient(
       189.44deg,
       rgba(0, 0, 0, 0) 58.48%,
-      rgba(0, 0, 0, 0.106473) 63.17%,
-      rgba(0, 0, 0, 0.235359) 68.85%,
-      rgba(0, 0, 0, 0.492821) 78.08%,
-      rgba(0, 0, 0, 0.740286) 85.86%,
-      #000000 92.87%
+      rgba(0, 0, 0, 0.106) 63.17%,
+      rgba(0, 0, 0, 0.235) 68.85%,
+      rgba(0, 0, 0, 0.494) 78.08%,
+      rgba(0, 0, 0, 0.74) 85.86%,
+      rgb(0, 0, 0) 92.87%
     ),
-    url(${({ backdrop }) => backdrop}) center center no-repeat;
+    url(${({ backdrop }) => backdrop});
+  background-size: 1920px 770px, 1920px 770px, 1920px 770px, 1920px 770px,
+    1368px 769px;
+  background-repeat: no-repeat;
+  background-position: top center;
+
+  @media (max-width: 1039.98px) {
+    background-size: 1040px 400px, 1040px 400px, 1040px 400px, 1040px 400px,
+      776px 400px;
+    background-position: center top;
+    height: 400px;
+  }
+
+  @media (max-width: 1199px) {
+    background-size: 1366px 525px, 1366px 525px, 1366px 525px, 1366px 525px,
+      1024px 525px;
+    height: 525px;
+  }
 `;
 
 export const MovieMainData = styled.div`
   color: ${({ theme }) => theme.colors.white};
   position: absolute;
-  bottom: 92px;
-  left: 276px;
-
-  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
-    bottom: 10px;
-    left: 16px;
-  }
+  bottom: 15%;
+  left: 10%;
 `;
 
 export const MovieTitle = styled.h2`
