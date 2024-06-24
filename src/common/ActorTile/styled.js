062230aabe37a03ed78a08bed5f22127159c3b9c
Sofya <124407697+sofyahreksoftware@users.noreply.github.com>
2024-06-24 23:00:25 +0200

    refactor: correct person tile width (#19)
    * refactor: remove unus unused files
    
    * feat: add adjustment of tiles to grid width
    
    * feat: add constant width to tiles

--- src/common/ActorTile/styled.js
+++ src/common/ActorTile/styled.js
@@ -1,32 +1,26 @@
 import styled from "styled-components";
 
 export const StyledActorTile = styled.section`
-  max-width: 208px;
-  min-height: 339px;
+  width: 100%;
+  height: 100%;
   padding: 16px;
-  text-align: center;
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 4px 12px 0px ${({ theme }) => theme.colors.heather};
+  text-align: center;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     padding: 8px;
     border-radius: 5px;
-    max-width: 136px;
-    min-height: 245px;
   }
 `;
 
 export const Image = styled.img`
   border-radius: 5px;
-  width: 176px;
-  height: 231px;
-  object-fit: cover;
-  margin-bottom: 12px;
+  margin: 0 auto 12px;
+  width: 100%;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
-    width: 120px;
-    height: 178px;
-    margin-bottom: 8px;
+    margin: 0 auto 8px;
   }
 `;
 
@@ -34,7 +28,7 @@ export const ActorName = styled.h3`
   line-height: 1.3;
   font-size: 22px;
   font-weight: 500;
-  margin: 0;
+  margin: 0 auto;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     line-height: 1.3;
