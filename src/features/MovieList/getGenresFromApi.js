e895a29ec6496ca41a4386724b3df9f5314827cb
Dawid Kasza <dawidkasza@op.pl>
2024-07-01 23:56:09 +0200

    add getGenresFromApi

---
+++ src/features/MovieList/getGenresFromApi.js
@@ -0,0 +1,9 @@
+export const getGenresFromApi = async (api) => {
+  const response = await fetch(api);
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return await response.json();
+};
