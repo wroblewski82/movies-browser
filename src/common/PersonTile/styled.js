634f02c2b042dfb3b9376a149b21bcfa7710894e
Sofya <sofigrekal@gmail.com>
2024-07-05 22:47:55 +0200

    refactor: improve styles of PersonTile for mobile

--- src/common/PersonTile/styled.js
+++ src/common/PersonTile/styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledPersonTile = styled.section`
   display: grid;
   grid-template-rows: 4fr 1fr;
+  gap: 1px;
   width: 100%;
   height: 100%;
   padding: 16px 16px 0;
@@ -10,7 +11,7 @@ export const StyledPersonTile = styled.section`
   text-align: center;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
-    padding: 8px;
+    padding: 8px 8px 8px 0;
     border-radius: 5px;
   }
 `;
@@ -30,6 +31,7 @@ export const PersonName = styled.h3`
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     line-height: 1.3;
     font-size: 14px;
+    margin: 8px auto 0;
   }
 `;
 
@@ -39,4 +41,10 @@ export const Role = styled.p`
   line-height: 1.5;
   color: ${({ theme }) => theme.colors.waterloo};
   margin: 8px auto 16px;
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    font-size: 13px;
+    line-height: 1.3;
+    margin: 8px auto 8px;
+  }
 `;
