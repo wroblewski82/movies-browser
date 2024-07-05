df7011058f6724fded8941e4b8b2451955a3ada1
Sofya <sofigrekal@gmail.com>
2024-07-05 22:41:57 +0200

    feat: improve personTile-styles

--- src/common/PersonTile/styled.js
+++ src/common/PersonTile/styled.js
@@ -3,10 +3,9 @@ import styled from "styled-components";
 export const StyledPersonTile = styled.section`
   display: grid;
   grid-template-rows: 4fr 1fr;
-  gap: 12px;
   width: 100%;
   height: 100%;
-  padding: 16px;
+  padding: 16px 16px 0;
   background-color: ${({ theme }) => theme.colors.white};
   text-align: center;
 
@@ -26,7 +25,7 @@ export const PersonName = styled.h3`
   line-height: 1.3;
   font-size: 22px;
   font-weight: 500;
-  margin: 0 auto;
+  margin: 12px auto 0;
 
   @media (max-width: ${({ theme }) => theme.media.mobile}px) {
     line-height: 1.3;
